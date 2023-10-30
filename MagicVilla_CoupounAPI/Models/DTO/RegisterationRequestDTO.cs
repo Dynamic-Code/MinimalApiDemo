535fa4df40e49ba329e85c8edb33ddc2001155ee
Abhinav Kumar <dynamic2106@gmail.com>
2023-10-30 22:48:36 +0530

    Created All the required Models for auth

---
+++ MagicVilla_CoupounAPI/Models/DTO/RegisterationRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MagicVilla_CoupounAPI.Models.DTO
+{
+    public class RegisterationRequestDTO
+    {
+        public int UserName { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+    }
+}
